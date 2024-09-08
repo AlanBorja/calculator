9ce29a144fde2443b585a0dfbe94acd1287f9e43
AlanBorja <177421166+AlanBorja@users.noreply.github.com>
2024-09-08 20:32:59 -0300

    Generando mas logica

--- calculating.js
+++ calculating.js
@@ -1,3 +1,8 @@
+/*
+1) Hacer que al clickear el boton se actualice el valor de resultado.
+
+*/
+
 function actualizarResultado(numero){
     let newResultado = document.querySelector('.resultado');
     newResultado.textContent += numero
@@ -9,10 +14,21 @@ function darResultado() {
     let evaluar = document.querySelector('.resultado').textContent;
     const numerosSplit = evaluar.split(/(\+|\-|\*\*|\/|\*|\(|\))/);
     let resultado = parseFloat(numerosSplit[0]);
+    let lastDigit;
     for (let i = 1; i < numerosSplit.length; i +=2) {
         const operador = numerosSplit[i];
         const siguienteNumero = parseFloat(numerosSplit[i + 1]);
-        console.log(operador)
+
+        if(numerosSplit[numerosSplit.length] == operador && i >= numerosSplit.length - 2){
+            lastDigit = operador;
+            console.log(lastDigit);
+            break
+
+        };
+        //Intentando hacer que si el ultimo digito es un operador, lo guarde como parte de resultado y lo use en la siguiente operacion, tiene que aparecer asi por ejemplo 2+2+ = 4+
+        console.log(numerosSplit)
+        console.log(numerosSplit[numerosSplit.length-2])
+        console.log(numerosSplit[numerosSplit.length] == operador)
         switch (operador) {
             case '+':
                 resultado += siguienteNumero;
